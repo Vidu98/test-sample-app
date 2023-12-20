34f7a6c7d9f618a9000d1a02ab3f21abef5f0e78
Vidumina Rathnayake <viduminarmvc@gmail.com>
2023-12-20 18:43:47 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello How are you I love you
         </a>
       </header>
     </div>
